dc374bfd70e68fac90838dbb77ce8698a6f5313e
Rudra Shekhar Nag <82301307+Rudra-2009@users.noreply.github.com>
2022-12-02 21:16:50 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function french() {
+    document.getElementById("heading").innerHTML="Blog de voyage en Inde";
+    document.getElementById("paragraph").innerHTML="Voici quelques villes en Inde. Cliquez sur l'un d'entre eux pour obtenir des informations à son sujet";
+}
+function english() {
+    document.getElementById("heading").innerHTML="India Travel Blog";
+    document.getElementById("paragraph").innerHTML="Here are a few cities in India.Click on one to gain some info about it ";
+}
